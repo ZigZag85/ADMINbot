512ed919ad2f21e9b14f0a983adcb36a1a8ed265
prgofficial <65294906+prgofficial@users.noreply.github.com>
2020-05-15 20:08:13 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,18 +18,15 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Hello {}, My name is {}! If you have any questions about how to use me, Click here > /help... 
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+I'm a Group Manager bot maintained by  [this person](tg://user?id={}).
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+This Bot is exclusively made for @moviesonlydiscussion ; мσνιєѕσиℓу™ Team 
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
+For more Details, contact> @prgofficial ...
 
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+**SO MANY MOVIES, SO LITTLE TIME!**
 
 """
 
